5eb246c113931e36dfb0af814bc26aa623e9a1b4
rhonabaun <baunrhonalyn1224@gmail.com>
2024-07-06 17:28:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,40 @@
-var product1 = document.getElementById("product1")
-            var qty1 = document.getElementById("qty1")
-            var price1 = document.getElementById("price1")
-
-            var product2 = document.getElementById("product2")
-            var qty2 = document.getElementById("qty2")
-            var price2 = document.getElementById("price2")
-
-            var carts = document.getElementById("carts")
-            var total = document.getElementById("total")
-            var cash = document.getElementById("cash")
-            var change = document.getElementById("change")
-
-            function addOrder() {
-                carts.textContent=""
-                if (parseFloat(qty1.value) > 0){
-                    var order = qty1.value.toString() + ' pc/s x ' + price1.textContent + '------'+ product1.textContent + '------ Php' + (parseFloat(qty1.value)*parseFloat(price1.textContent)) + '\n'
-                    //carts.textContent += carts.value.toString() + "\n";
-                    carts.textContent += order
-                }
-                if (parseFloat(qty2.value) > 0){
-                    var order = qty2.value.toString() + ' pc/s x ' + price2.textContent + '------' + product2.textContent + '------ Php' + (parseFloat(qty2.value)*parseFloat(price2.textContent)) + '\n'
-                    //carts.textContent += carts.value.toString() + "\n";
-                    carts.textContent += order
-            }
-           if (parseFloat(qty3.value) > 0){
-                    var order = qty3.value.toString() + ' pc/s x ' + price3.textContent + '------' + product3.textContent + '------ Php' + (parseFloat(qty3.value)*parseFloat(price3.textContent)) + '\n'
-                    //carts.textContent += carts.value.toString() + "\n";
-                    carts.textContent += order
-            }
-            if (parseFloat(qty4.value) > 0){
-                    var order = qty4.value.toString() + ' pc/s x ' + price4.textContent + '------' + product4.textContent + '------ Php' + (parseFloat(qty4.value)*parseFloat(price4.textContent)) + '\n'
-                    //carts.textContent += carts.value.toString() + "\n";
-                    carts.textContent += order
-            }
-            if (parseFloat(qty5.value) > 0){
-                    var order = qty5.value.toString() + ' pc/s x ' + price5.textContent + '------' + product5.textContent + '------ Php' + (parseFloat(qty5.value)*parseFloat(price5.textContent)) + '\n'
-                    //carts.textContent += carts.value.toString() + "\n";
-                    carts.textContent += order
-            }
-            if (parseFloat(qty6.value) > 0){
-                    var order = qty6.value.toString() + ' pc/s x ' + price6.textContent + '------' + product6.textContent + '------ Php' + (parseFloat(qty6.value)*parseFloat(price6.textContent)) + '\n'
-                    //carts.textContent += carts.value.toString() + "\n";
-                    carts.textContent += order
-            }
-            }
-            qty1.addEventListener("keyup", addOrder);
-            qty2.addEventListener("keyup", addOrder);
-            qty3.addEventListener("keyup", addOrder);
-            qty4.addEventListener("keyup", addOrder);
-            qty5.addEventListener("keyup", addOrder);
-            qty6.addEventListener("keyup", addOrder);
+const btnLike1 = document.getElementById("btnLike1")
+const countLikes1 = document.getElementById("countLikes1")
+const btnLike2 = document.getElementById("btnLike2")
+const countLikes2 = document.getElementById("countLikes2")
+const btnDislike1 = document.getElementById("btnDislike1")
+const countDislike1 = document.getElementById("countDislike1")
+const btnDislike2 = document.getElementById("btnDislike2")
+const countDislike2 = document.getElementById("countDislike2")
+const submit = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
+function clickLike1(){
+    let totalLikes1 = parseInt(countLikes1.value) + 1
+    countLikes1.textContent = totalLikes1.toString()
+  }
+function clickDislike1(){
+  let totalDislikes1 = parseInt(countDislikes1.value) + 1
+  countDislikes1.textContent = totalDislikes1.toString()
+}
+function clickLike2(){
+    let totalLikes2 = parseInt(countLikes2.value) + 1
+    countLikes2.textContent = totalLikes2.toString()
+}
+function clickDislike2(){
+  let totalDislikes2 = parseInt(countDislikes2.value) + 1
+  countDislikes2.textContent = totalDislikes2.toString()
+}
+function clickheart1(){
+    let totalLikes1 = parseInt(countheart1.value) + 1
+    countheart1.textContent = totalLikes1.toString()
+  }
+function submitComment(){
+commentbox.textContent += comment.value.toString() + "\n"
+comment.value="";
+}
+btnLike1.addEventListener("click",clickLike1)
+btnLike2.addEventListener("click",clickLike2)
+btnDislike1.addEventListener("click",clickDislike1)
+btnDislike2.addEventListener("click",clickDislike2)
+submit.addEventListener("click",submitComment)
